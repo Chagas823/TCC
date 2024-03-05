6b635c0b79f17d567796a2090f6ecba032afe3b6
Francisco das Chagas <franciscodaschagas823@gmail.com>
2024-03-05 14:36:09 -0300

    teste

--- api/src/linkabe/index.js
+++ api/src/linkabe/index.js
@@ -1,12 +1,14 @@
-let lrs = require("./lrs/index");
+let lrs = require("lrs/index");
 let chagas = lrs.gen();
 let vicente = lrs.gen();
 let sales = lrs.gen();
 let grupo = [chagas.publicKey, vicente.publicKey, sales.publicKey];
 
-let assinatura = lrs.sign(grupo, sales, "Sales é feio");
-// Anyone is able to verify *some* of them signed that message
-console.log(lrs.verify(grupo, assinatura, "Sales é feio"));
-let assinatura2 = lrs.sign(grupo, sales, "Vicente é feio");
+// assina determinada mensagem
 
+let assinatura = lrs.sign(grupo, sales, "Sales é feio");
+// verifica se o grupo assinou determinada assinatura com determinada mensagem
+console.log(lrs.verify(grupo, assinatura, "Sales é feio"));
+let assinatura2 = lrs.sign(grupo, chagas, "Vicente é feio");
+console.log(chagas)
 console.log(lrs.link(assinatura, assinatura2))
